171f41076724bd2b39e55385cbd6ee937453fe33
565102412@qq.com <565102412@qq.com>
2015-12-11 17:27:46 +0800

    文件遗漏

---
+++ HaierAppTest/Common/NetworkConnection.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Net;
+
+namespace HaierAppTest
+{
+
+    public class NetworkConnection: INetworkConnection
+    {
+        public bool IsConnected { get; set; }
+        public void CheckNetworkConnection()
+        {
+            var connectivityManager = (ConnectivityManager)Application.Context.GetSystemService(Context.ConnectivityService);
+            var activeNetworkInfo = connectivityManager.ActiveNetworkInfo;
+            if (activeNetworkInfo != null && activeNetworkInfo.IsConnectedOrConnecting)
+            {
+                IsConnected = true;
+            }
+            else
+            {
+                IsConnected = false;
+            }
+        }
+    }
+}
